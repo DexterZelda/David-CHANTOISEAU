8f8063db89635b715da7de8311ebb3c4de4cbc15
David CHANTOISEAU <gogs@fake.local>
2020-01-22 13:15:32 +0100

    Version 0.7 Pokemon Ok

--- Projet2/PokemonManager/src/app/composants/generations/generations.component.ts
+++ Projet2/PokemonManager/src/app/composants/generations/generations.component.ts
@@ -5,38 +5,16 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './generations.component.html',
   styleUrls: ['./generations.component.css']
 })
+
 export class GenerationsComponent implements OnInit {
 
   // Variables
-  private unAttribut = "Générations de POKEMONS";
-  private tabPokemons = [];
-  private compteur = 0;
-  private nbPokemon = 0;
+  private _titre = "Générations de POKEMONS";
 
   // Constructeur
   constructor() { }
 
   // Méthodes
-  ajouterPlusUn() {
-    this.compteur++;
-    console.log(this.compteur);
-  }
-
-  resetCompteur() {
-    this.compteur = 0;
-  }
-
-  ajouterPokemon() {
-    this.nbPokemon++;
-    let tmpNom = "";
-    tmpNom += "Pokemon"+this.nbPokemon.toString();
-    this.tabPokemons.push(tmpNom);
-  }
-
-  enleverPokemon() {
-    this.tabPokemons.pop();
-    this.nbPokemon--;
-  }
 
   ngOnInit() { }
 }
--- Projet2/PokemonManager/src/app/composants/pokemons/pokemons.component.ts
+++ Projet2/PokemonManager/src/app/composants/pokemons/pokemons.component.ts
@@ -5,11 +5,38 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './pokemons.component.html',
   styleUrls: ['./pokemons.component.css']
 })
-export class PokemonsComponent implements OnInit {
 
+export class PokemonsComponent implements OnInit {
+  // Variables
+  private _titre = "Ajouter des POKEMONS";
+  private _tabPokemons = [];
+  private _compteur = 0;
+  private _nbPokemon = 0;
+
+  // Constructeur
   constructor() { }
 
-  ngOnInit() {
+  // Méthodes
+  ajouterPlusUn() {
+    this._compteur++;
+    console.log(this._compteur);
   }
 
+  resetCompteur() {
+    this._compteur = 0;
+  }
+
+  ajouterPokemon() {
+    this._nbPokemon++;
+    let tmpNom = "";
+    tmpNom += "Pokemon"+this._nbPokemon.toString();
+    this._tabPokemons.push(tmpNom);
+  }
+
+  enleverPokemon() {
+    this._tabPokemons.pop();
+    this._nbPokemon--;
+  }
+
+  ngOnInit() { }
 }
