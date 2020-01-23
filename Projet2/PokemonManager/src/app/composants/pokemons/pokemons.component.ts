ca09e568d64e05c3060d963abceb764e98065d64
David CHANTOISEAU <gogs@fake.local>
2020-01-23 12:49:25 +0100

    Version 0.9 Stable - Pokemon Ok

--- Projet2/PokemonManager/src/app/composants/pokemons/pokemons.component.ts
+++ Projet2/PokemonManager/src/app/composants/pokemons/pokemons.component.ts
@@ -15,6 +15,8 @@ export class PokemonsComponent implements OnInit {
   // Nombre de Pokemon total (22/01/2020 - avec Génération 8)
   private NB_POKEMON = 890;
   private _monPokemonPrefered = 'aucun';
+  // Pokemons (0:id, 1:img, 2:nom, 3:géné, 4:cat, 5:type, 6:taille, 7:poids)
+  private _tabMonPrefered = [];
 
   // Constructeur
   constructor() {
@@ -30,9 +32,10 @@ export class PokemonsComponent implements OnInit {
     // Image des différents Pokemons
     const imagePokemon = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
       + this._nbPokemon.toString() + '.png';
-    // Ajout des Pokemons
+    // Ajout des Pokemons (0:id, 1:img, 2:nom, 3:géné, 4:cat, 5:type, 6:taille, 7:poids)
     const tabNewPokemon =
-      [imagePokemon, tmpNom, 'Génération x', ' Catégorie : graine', ' Type : feu', ' Taille : 0.7 m', ' Poids : 22 kg'];
+      [this._nbPokemon, imagePokemon, tmpNom, 'Génération x', ' Catégorie : graine',
+        ' Type : feu', ' Taille : 0.7 m', ' Poids : 22 kg'];
     this._tabPokemons.push(tabNewPokemon);
   }
 
@@ -60,12 +63,25 @@ export class PokemonsComponent implements OnInit {
       // récursivité
       this.enleverAllPokemon();
     }
-    this._monPokemonPrefered = 'aucun';
   }
 
   // Mettre à jour le Pokemon préféré
   majPokemonPrefered(valeur) {
-    this._monPokemonPrefered = valeur;
+    let ok = false, i = 0;
+    do {
+      if (this._tabPokemons[i][0] == valeur) {
+        this._tabMonPrefered = this._tabPokemons[i];
+        this._monPokemonPrefered = this._tabMonPrefered[2];
+        ok = true;
+      }
+      i++;
+    } while (!ok);
+  }
+
+  // Enlever le Pokemon préféré
+  enleverPokemonPrefered() {
+    this._tabMonPrefered[0] = null;
+    this._monPokemonPrefered = 'aucun';
   }
 
   ngOnInit() {
